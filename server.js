07bbb05e91c2209c59fde63ca190586c00e8650a
Tucker Beauchamp <tuckerbeauchamp@gmail.com>
2021-06-30 10:12:55 -0700

    auto deploy

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get("*", function (req, res) {
-  res.send("Welcome to COUSCOUS");
+  res.send("Hopefully this automatic deploy works!");
 });
 
 app.listen(PORT, () => console.log("App listening on port " + PORT));
